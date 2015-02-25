366baacc6176afc0995489858814dbb3debcb31c
altcavs <altcavs@mail.ru>
2015-02-25 19:26:51 +0500

    add content

---
+++ WebInfo/Models/Domain/Content.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace WebInfo.Models.Domain
+
+{
+    public class Content : Entity<object>
+    {
+        public Content()
+        {
+
+        }
+        public String Title { get; set; }
+        public Boolean Pinned { get; set; }
+        public DateTime Published_at { get; set; }
+
+        public Category Category {get; set;}
+
+    }
+}
